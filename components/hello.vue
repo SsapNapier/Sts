297c0432d9182faa727499b2eac249cf8791de5f
SsapNapier <48889205+SsapNapier@users.noreply.github.com>
2019-04-01 17:28:57 +0300

    Add files via upload

---
+++ components/hello.vue
@@ -0,0 +1,18 @@
+<template>
+      <h1>{{ greeting }}</h1>
+</template>
+
+<script>
+    module.exports = {
+        data: function() {
+           return {
+               greeting: 'Hello!'
+           }
+        }
+    }
+</script>
+
+<style lang="sass?indentedSyntax=true">
+    h1
+        color:blue
+</style>
